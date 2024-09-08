80d1ed48ba8729e3321fa32a03ca152f07271bb1
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-09-08 12:43:47 +0200

    add types/todo.ts, display todoList

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,33 @@
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  ScrollView,
+} from 'react-native';
 import { useState } from 'react';
+import { Todo } from '../types/todo';
 
 export default function Page() {
   const [todo, setTodo] = useState<string>('todo item');
+  const [todoList, setTodoList] = useState<Todo[]>([
+    {
+      id: '1',
+      description: 'item 1',
+      isCompleted: false,
+    },
+    {
+      id: '2',
+      description: 'item 2',
+      isCompleted: false,
+    },
+    {
+      id: '3',
+      description: 'item 3',
+      isCompleted: true,
+    },
+  ]);
 
   return (
     <View style={styles.container}>
@@ -12,7 +37,13 @@ export default function Page() {
         <Button title="add todo" onPress={() => console.log(`${todo}`)} />
       </View>
       {/* List */}
-      <View></View>
+      <ScrollView>
+        {todoList.map((todo) => (
+          <View>
+            <Text>{todo.description}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
